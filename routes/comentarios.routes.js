c01ab789a669a734815b8fb2cdd26684c6b52ac7
Santiago Maza <santiagommaza@gmail.com>
2024-03-23 00:35:26 -0300

    Se hicieron validaciones con express validator en routes y controller de comentarios

--- routes/comentarios.routes.js
+++ routes/comentarios.routes.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const comentario = express.Router()
 const { obtenerComentarios, crearComentario, modificarComentario, borrarComentario } = require('../controllers/comentarios.controller')
+const { jwtValidator } = require('../middlewares/jwt')
+const { body } = require('express-validator')
 
 comentario.get('/obtener-comentarios', obtenerComentarios)
 
-comentario.post('/crear-comentario', crearComentario)
+comentario.post('/crear-comentario', 
+  body("usuario").exists().escape().trim().isAlphanumeric("es-ES", { ignore: ' ' }).not().isEmpty().withMessage("Usuario invalido"),
+  body("producto").exists().escape().trim().isAlphanumeric("es-ES", { ignore:'' }).not().isEmpty().withMessage("Producto invalido"),
+  body("mensaje").exists().escape().trim().isAlphanumeric("es-ES", { ignore:'' }).not().isEmpty().withMessage("Comentario invalido"),
+  body("fecha").exists().escape().trim().isDate({format: "MM/DD/YYYY"}).isAlphanumeric("es-ES", { ignore:'' }).not().isEmpty().withMessage("Fecha invalida"),
+  jwtValidator,
+crearComentario)
 
-comentario.patch('/modificar-comentario', modificarComentario)
+comentario.patch('/modificar-comentario', 
+  body("usuario").exists().escape().trim().isAlphanumeric("es-ES", { ignore: ' ' }).not().isEmpty().withMessage("Usuario invalido"),
+  body("producto").exists().escape().trim().isAlphanumeric("es-ES", { ignore:'' }).not().isEmpty().withMessage("Producto invalido"),
+  body("mensaje").exists().escape().trim().isAlphanumeric("es-ES", { ignore:'' }).not().isEmpty().withMessage("Comentario invalido"),
+  body("fecha").exists().escape().trim().isDate({format: "MM/DD/YYYY"}).isAlphanumeric("es-ES", { ignore:'' }).not().isEmpty().withMessage("Fecha invalida"),
+jwtValidator, modificarComentario)
 
-comentario.delete('/borrar-comentario', borrarComentario)
+comentario.delete('/borrar-comentario', jwtValidator,  borrarComentario)
 
 module.exports = comentario
